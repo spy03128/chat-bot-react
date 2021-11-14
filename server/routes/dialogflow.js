c03c58d2a31913549357c4e1ede8c098ab6edd9f
지현 <spy03128@naver.com>
2021-11-14 18:03:12 +0900

    챗봇 파일 구조 생성

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const structjson = require("./structjson.js");
 
+//We will make two Route
+
 //Text Query Route
 
 //Event Query Route
