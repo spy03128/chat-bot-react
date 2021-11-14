82a5fb454ff8d374a418ce3432d7a0a2201a912c
지현 <spy03128@naver.com>
2021-11-14 18:49:34 +0900

    Text Query 만들기

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -1,10 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const structjson = require("./structjson.js");
+const dialogflow = require("dialogflow");
+const uuid = require("uuid");
+
+const config = require("../config/keys");
+
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
+
+// Create a new session
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
 //We will make two Route
 
 //Text Query Route
+
+router.post("/textQuery", async (req, res) => {
+  //클라이언트에서 받은 정보 dialogflow에 전달
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: req.body.text,
+        // The language used by the client (en-US)
+        languageCode: languageCode,
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+
+  res.send(result);
+});
 
 //Event Query Route
 
