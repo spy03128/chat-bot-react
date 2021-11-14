972801e7bb98914c5094cef76be84c13392cbcfc
지현 <spy03128@naver.com>
2021-11-14 22:13:03 +0900

    챗봇 UI 제작

---
+++ client/src/Chatbot/Chatbot.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Chatbot() {
+  const keyPressHanlder = (e) => {
+    if (e.key === "Enter") {
+      if (!e.target.value) {
+        return alert("you need to type something first");
+      }
+
+      //   textQuery(e.target.value);
+
+      e.target.value = "";
+    }
+  };
+
+  return (
+    <div
+      style={{
+        height: 700,
+        width: 700,
+        border: "3px solid black",
+        borderRadius: "7px",
+      }}
+    >
+      <div style={{ height: 644, width: "100%", overflow: "auto" }}></div>
+
+      <input
+        style={{
+          margin: 0,
+          width: "100%",
+          height: 50,
+          borderRadius: "4px",
+          padding: "5px",
+          fontSize: "1rem",
+        }}
+        placeholder="Send a message..."
+        onKeyPress={keyPressHanlder}
+        type="text"
+      />
+    </div>
+  );
+}
+
+export default Chatbot;
