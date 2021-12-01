7e519b63217df2a530eb13ab8e8ba33582ee2521
지현 <spy03128@naver.com>
2021-12-01 17:24:20 +0900

    textQuery 만들기

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,13 +1,64 @@
+import Axios from "axios";
 import React from "react";
 
 function Chatbot() {
+  const textQuery = async (text) => {
+    // let converstaions = [];
+
+    //I sent
+    let conversation = {
+      who: "user",
+      content: {
+        text: {
+          text: text,
+        },
+      },
+    };
+
+    // conversations.push(converstaion);
+
+    //Chatbot sent
+
+    const textQueryVariable = {
+      text,
+    };
+
+    try {
+      //I will send request to the text Query ROUTE
+
+      const response = await Axios.post(
+        "/api/dialogflow/textQuery",
+        textQueryVariable
+      );
+      const content = response.data.fulfillmentMessages[0];
+
+      conversation = {
+        who: "bot",
+        content: content,
+      };
+      // conversations.push(converstaion);
+      console.log(conversation);
+    } catch (error) {
+      conversation = {
+        who: "bot",
+        content: {
+          text: {
+            text: "Error just occured, please check the problem",
+          },
+        },
+      };
+      console.log(conversation);
+      // conversations.push(converstaion);
+    }
+  };
+
   const keyPressHanlder = (e) => {
     if (e.key === "Enter") {
       if (!e.target.value) {
         return alert("you need to type something first");
       }
 
-      //   textQuery(e.target.value);
+      textQuery(e.target.value);
 
       e.target.value = "";
     }
@@ -22,7 +73,10 @@ function Chatbot() {
         borderRadius: "7px",
       }}
     >
-      <div style={{ height: 644, width: "100%", overflow: "auto" }}></div>
+      <div style={{ height: 644, width: "100%", overflow: "auto" }}>
+        {" "}
+        conversations
+      </div>
 
       <input
         style={{
